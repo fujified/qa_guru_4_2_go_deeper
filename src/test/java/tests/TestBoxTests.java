e0b6469f25b50b7fbe42580825b4d7b42c066026
fujified <uktras@gmail.com>
2021-01-31 03:20:19 +0300

    selenide

---
+++ src/test/java/tests/TestBoxTests.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBoxTests {
+
+    @Test
+    void dataAppearsInOutputBlockTest() {
+        String name = "Bob";
+        String email = "bob@gmail.com";
+        String currentAddress = "Nevsky";
+        String permanentAddress = "Market";
+
+        open("https://demoqa.com/text-box");
+
+        $(".main-header").shouldHave(text("Text Box"));
+
+        $("#userName").setValue(name);
+        $("#userEmail").setValue(email);
+        $("#currentAddress").setValue(currentAddress);
+        $("#permanentAddress").setValue(permanentAddress);
+        $("submit").click();
+
+        $("#output").shouldHave(text(name), text(email), text(currentAddress), text(permanentAddress));
+    }
+}
